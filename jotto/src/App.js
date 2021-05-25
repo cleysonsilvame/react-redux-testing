51978821e0c265fe00d15e281b681ad0cb58d431
Cleyson Silva <cleysonsilva.me@gmail.com>
2021-05-25 14:33:46 -0300

    feat: inicio do projeto jotto

---
+++ jotto/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+
+
+  render() {
+    return (
+      <div data-test="component-app">
+        <h1>hello world</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
