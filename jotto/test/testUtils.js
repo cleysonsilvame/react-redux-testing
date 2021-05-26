c3d67e6a944a4915fb884d679be5ff9835b389b2
Cleyson Silva <cleysonsilva.me@gmail.com>
2021-05-25 23:46:32 -0300

    add: Tipos para o componente Congrats

--- jotto/test/testUtils.js
+++ jotto/test/testUtils.js
@@ -1,3 +1,5 @@
+import checkPropTypes from 'check-prop-types';
+
 /**
  * Return node(s) with the given data-test attribute.
  * @param {ShallowWrapper} wrapper - Enzyme shallow wrappers.
@@ -6,4 +8,15 @@
  */
 export const findByTestAttr = (wrapper, val) => {
   return wrapper.find(`[data-test="${val}"]`);
+};
+
+export const checkProp = (component, conformingProps) => {
+  const propError = checkPropTypes(
+    component.prototype,
+    conformingProps,
+    'prop',
+    component.name
+  );
+
+  return propError;
 };
