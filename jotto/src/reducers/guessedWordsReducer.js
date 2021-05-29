232cf33935bfbff901d88acbc5c30be39b113c1e
Cleyson Silva <cleysonsilva.me@gmail.com>
2021-05-29 20:12:32 -0300

    feat: guessed words reducer

---
+++ jotto/src/reducers/guessedWordsReducer.js
@@ -0,0 +1,9 @@
+/**
+ * @function guessedWordsReducer
+ * @param {array} state - Array of guessed words.
+ * @param {object} action - action to be reduced.
+ * 
+ */
+export default (state, action) => {
+  return null
+}
