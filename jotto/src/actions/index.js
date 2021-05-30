2b4dae4c693ee06b858d2d7f7e0439f5202295e5
Cleyson Silva <cleysonsilva.me@gmail.com>
2021-05-30 04:35:44 -0300

    add: axios e moxios

--- jotto/src/actions/index.js
+++ jotto/src/actions/index.js
@@ -1,8 +1,10 @@
-import {getLetterMatchCount} from '../helpers'
+import axios from "axios";
+import { getLetterMatchCount } from "../helpers";
 
 export const actionTypes = {
   CORRECT_GUESS: "CORRECT_GUESS",
   GUESS_WORD: "GUESS_WORD",
+  SET_SECRET_WORD: "SET_SECRET_WORD",
 };
 
 /**
@@ -14,15 +16,21 @@ export const actionTypes = {
 export const guessWord = guessedWord => {
   return function(dispatch, getState) {
     const secretWord = getState().secretWord;
-    const letterMatchCount = getLetterMatchCount(guessedWord, secretWord)
+    const letterMatchCount = getLetterMatchCount(guessedWord, secretWord);
 
     dispatch({
       type: actionTypes.GUESS_WORD,
-      payload: {guessedWord, letterMatchCount}
-    })
+      payload: { guessedWord, letterMatchCount },
+    });
 
     if (guessedWord === secretWord) {
-      dispatch({type: actionTypes.CORRECT_GUESS})
+      dispatch({ type: actionTypes.CORRECT_GUESS });
     }
   };
 };
+
+export const getSecretWord = () => {
+  return dispatch => {
+    axios.get("http://localhost:3030")
+  }
+}
