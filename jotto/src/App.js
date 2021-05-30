e0292fdbabc48b4e3f77b707b9000d25f1c2f144
Cleyson Silva <cleysonsilva.me@gmail.com>
2021-05-30 05:30:26 -0300

    add: conecta redux no app

--- jotto/src/App.js
+++ jotto/src/App.js
@@ -1,20 +1,25 @@
-import React, { Component } from 'react';
-import './App.css';
-import GuessedWords from './GuessedWords';
-import Congrats from './Congrats';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { getSecretWord } from "./actions";
+import "./App.css";
+import Congrats from "./Congrats";
+import GuessedWords from "./GuessedWords";
+import Input from "./Input";
 
 class App extends Component {
   render() {
     return (
       <div data-test="component-app" className="container">
         <h1>Jotto</h1>
-        <Congrats success={true} />
-        <GuessedWords guessedWords={[
-          { guessedWord: 'train', letterMatchCount: 3}
-        ]} />
+        <Congrats success={this.props.success} />
+        <Input />
+        <GuessedWords guessedWords={this.props.guessedWords} />
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = ({ success, guessedWords, secretWord }) => {
+  return { success, guessedWords, secretWord };
+};
+export default connect(mapStateToProps, { getSecretWord })(App);
