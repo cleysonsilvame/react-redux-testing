510938c9a3c80b3c28378a7f8e5d703757f9c10f
Cleyson Silva <cleysonsilva.me@gmail.com>
2021-05-29 21:03:22 -0300

    feat: integração das guessedWords no Redux Thunk

--- jotto/src/reducers/guessedWordsReducer.js
+++ jotto/src/reducers/guessedWordsReducer.js
@@ -1,9 +1,16 @@
+import { actionTypes } from "../actions";
+
 /**
  * @function guessedWordsReducer
  * @param {array} state - Array of guessed words.
  * @param {object} action - action to be reduced.
- * 
+ *
  */
-export default (state, action) => {
-  return null
-}
+export default (state = [], action) => {
+  switch (action.type) {
+    case actionTypes.GUESS_WORD:
+      return [...state, action.payload];
+    default:
+      return state;
+  }
+};
