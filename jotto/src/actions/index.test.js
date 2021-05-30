872c9a36b66f87f11d8409bb5a32ac8a1db142bc
Cleyson Silva <cleysonsilva.me@gmail.com>
2021-05-30 04:50:21 -0300

    feat: testes com axios e moxios

--- jotto/src/actions/index.test.js
+++ jotto/src/actions/index.test.js
@@ -1,5 +1,8 @@
 import moxios from "moxios";
 
+import { storeFactory } from "../../test/testUtils";
+import { getSecretWord } from "./";
+
 describe("getSecretWord action creator", () => {
   beforeEach(() => {
     moxios.install();
@@ -9,5 +12,22 @@ describe("getSecretWord action creator", () => {
     moxios.uninstall();
   });
 
-  test("adds response word to state", () => {});
+  test("adds response word to state", () => {
+    const secretWord = "party";
+    const store = storeFactory();
+
+    moxios.wait(() => {
+      const request = moxios.requests.mostRecent();
+      request.respondWith({
+        status: 200,
+        response: secretWord,
+      });
+    });
+
+    return store.dispatch(getSecretWord()).then(() => {
+      const newState = store.getState();
+
+      expect(newState.secretWord).toBe(secretWord);
+    });
+  });
 });
