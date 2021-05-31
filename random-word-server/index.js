f24211c8ffc637d70d0f683fb0c39b055b1a63bb
Cleyson Silva <cleysonsilva.me@gmail.com>
2021-05-31 15:31:01 -0300

    fix: ajustes finais

--- random-word-server/index.js
+++ random-word-server/index.js
@@ -1,5 +1,6 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
+const cors = require('cors')
 const fs = require('fs');
 
 const fileContents = fs.readFileSync('./data.json', 'utf-8');
@@ -7,14 +8,15 @@ const words = JSON.parse(fileContents);
 const { fiveLetterWords } = words;
 
 
+server.use(cors());
 server.use(jsonServer.bodyParser);
-server.get('/word', (req, res) => {
+server.get('/', (req, res) => {
   const word =
     fiveLetterWords[Math.floor(Math.random() * fiveLetterWords.length)];
 
-  res.json({ word });
+  res.send(word);
 });
 
-server.listen(3333, () => {
+server.listen(3030, () => {
   console.log('JSON Server is running');
 });
